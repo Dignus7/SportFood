0c4c13916b7816d242aa9d502a3518a308830696
vitoss <vetaluhach@gmail.com>
2021-11-24 19:25:34 +0200

    add Menu Item

--- js/script.js
+++ js/script.js
@@ -168,21 +168,96 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  setClock('.timer', deadline); // -*******************MODAL************************
+  setClock('.timer', deadline); // -*******************MODAL************************************************************************
 
   const modalTrigger = document.querySelectorAll(' [data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
+
+  function openModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach(btn => {
     btn.addEventListener('click', () => {
-      modal.classList.toggle('show');
-      document.body.style.overflow = 'hidden';
+      openModal();
     });
   });
-  modalCloseBtn.addEventListener('click', () => {
-    modal.classList.toggle('show');
+
+  function closeModal() {
+    modal.classList.remove('show');
     document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      closeModal();
+    }
   });
+  document.addEventListener('keydown', e => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+    }
+  }); // const modalTimerId = setTimeout(openModal, 7000);
+
+  function ShowModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener("scroll", ShowModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', ShowModalByScroll); // -******************************************Tittle*********************************************************
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = +this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+
+  }
+
+  new MenuCard("img/tabs/vegy.jpg", "vegy", "Меню 'Фитнес'", 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, '.menu .container').render();
+  new MenuCard("img/tabs/elite.jpg", "elite", "Меню 'Премиум'", 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 15, '.menu .container').render();
+  new MenuCard("img/tabs/post.jpg", "post", "Меню 'Постное'", 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 13, '.menu .container').render(); // -**********************************************Slider2******************************************************
+  // const   sliderPrev = document.querySelector('.offer__slider-prev'),
+  //         sliderNext = document.querySelector('.offer__slider-next'),
+  //         sliderNum  = document.querySelector('current'),
+  //         sliderImg  = document.querySelectorAll('.offer__slide');
+  // sliderImg.forEach( item => {
+  //     item[0].classList.add('show');
+  //     item[1, 2, 3].classList.remove('show');
+  // } )
 });
 
 /***/ })
